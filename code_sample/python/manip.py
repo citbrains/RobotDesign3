75446bf7b2bb9be8275f55fac530a12026dbe7ea
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2019-01-25 15:57:15 +0900

    fix sign bug in main

--- code_sample/python/manip.py
+++ code_sample/python/manip.py
@@ -16,16 +16,13 @@ def send_angles(j,wait):
         f.flush()
 
     os.rename("/run/shm/angles.tmp","/run/shm/angles")
-    time.sleep(wait)
 
 def air_toggle(flag,wait):
     with open("/run/shm/ev_on_off.tmp","w") as air_f:
         air_f.write("1\n" if flag else "0\n")
         air_f.flush()
 
-    # os.rename("./ev_on_off.tmp","./ev_on_off")
     os.rename("/run/shm/ev_on_off.tmp","/run/shm/ev_on_off")
-    time.sleep(wait)
 
 def j_k(th1,th2,th3,th4,th5):
     th1,th2,th3,th4,th5 = radians(th1),radians(th2),radians(th3),radians(th4),radians(th5)
@@ -39,15 +36,16 @@ def j_k(th1,th2,th3,th4,th5):
     print(q)
     return q
 
-def i_k(x,y,z,a,b,c):
-    th1 = radians(a)
+def i_k(x,y,z,a,b,c,th1_flag,th3_flag):
+    th1 = radians(a) * (1 if th1_flag else -1)
     th5 = radians(c)
     b = radians(b)
     cos3 = (((z-L1-(L4+L)*cos(b))**2+(((x-cos(th1)*(L4+L)*sin(b))**2)+(y-sin(th1)*(L4+L)*sin(b))**2))-L2**2-L3**2)/(2*L3*L2)
-    sin3 = (1-cos3**2)**0.5
+    sin3 = (1-cos3**2)**0.5 * (1 if th3_flag else -1)
     th3 = atan2(sin3,cos3)
-    cos2 = ((z-L1-(L4+L)*cos(b))*(L2+L3*cos3)+sqrt(((x-cos(th1)*(L4+L)*sin(b))**2)+(y-sin(th1)*(L4+L)*sin(b))**2)*L3*sin3)
-    sin2 = (-(z-L1-(L4+L)*cos(b))*L3*sin3+sqrt(((x-cos(th1)*(L4+L)*sin(b))**2)+(y-sin(th1)*(L4+L)*sin(b))**2)*(L2+L3*cos3))
+    flaged_sign = 1 if (cos(th1)*x>=0 and sin(th1)*y>=0) else -1
+    cos2 = ((z-L1-(L4+L)*cos(b))*(L2+L3*cos3)+flaged_sign*sqrt(((x-cos(th1)*(L4+L)*sin(b))**2)+(y-sin(th1)*(L4+L)*sin(b))**2)*L3*sin3)
+    sin2 = (-(z-L1-(L4+L)*cos(b))*L3*sin3+flaged_sign*sqrt(((x-cos(th1)*(L4+L)*sin(b))**2)+(y-sin(th1)*(L4+L)*sin(b))**2)*(L2+L3*cos3)) * (1 if th1_flag else -1)
     th2 = atan2(sin2,cos2)
     th4 = b - th2 - th3
     th = [degrees(th1),degrees(th2),degrees(th3),degrees(th4),degrees(th5)] 
@@ -55,16 +53,22 @@ def i_k(x,y,z,a,b,c):
     return th
 
 if __name__ == "__main__" :
-    air_toggle(True,2.0)
-    send_angles(i_k(0,0,L1+L2+L3+L4+L,0,0,-86),3.0)
-    send_angles(i_k(269,0,291,0,180,-86),1.0)
-    send_angles(i_k(338,0,262,0,140,-86),2.0)
-    send_angles(i_k(368,0,250,0,100,-86),2.0)
-    send_angles(i_k(389,0,180,0,90,-86),2.0)
-    air_toggle(False,2.0)
-    send_angles(i_k(395,0,250,0,90,-86),2.0)
-    send_angles(i_k(338,0,262,0,90,-86),2.0)
-    send_angles(i_k(338,0,262,90,90,-86),2.0)
-    send_angles(i_k(395,0,250,90,90,-86),2.0)
-    air_toggle(True,2.0)
+    # air_toggle(True,2.0)
+    # send_angles(i_k(0,0,L1+L2+L3+L4+L,0,0,8,True,True),3.0)
+    # send_angles(i_k(269,0,291,0,180,8,True,True),1.0)
+    # send_angles(i_k(338,0,262,0,140,8,True,True),2.0)
+    # send_angles(i_k(368,0,250,0,100,8,True,True),2.0)
+    # send_angles(i_k(389,0,180,0,90,8,True,True),2.0)
+    # air_toggle(False,2.0)
+    # send_angles(i_k(395,0,250,0,90,8,True,True),2.0)
+    # send_angles(i_k(338,0,262,0,90,8,True,True),2.0)
+    # send_angles(i_k(338,0,262,90,90,8,True,True),2.0)
+    # send_angles(i_k(0,200,500,90,30,8,True,True),2.0)
+    # send_angles(i_k(0,-482,300,90,-90,8,True,False),2.0)
+    # send_angles(i_k(0,482,300,90,90,8,True,True),2.0)
+    # air_toggle(True,2.0)
+
+    send_angles(i_k(482,0,300,0,90,8,True,True),2.0)
+    send_angles(i_k(-482,0,300,0,-90,8,True,True),2.0)
+
 
