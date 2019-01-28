f02afdedde0a5d01366a4124230b368e77a437d7
matsumotokoki <s17c1124br@s.chibakoudai.jp>
2019-01-28 12:39:10 +0900

    throw

--- code_sample/python/manip.py
+++ code_sample/python/manip.py
@@ -3,19 +3,19 @@
 import sys,os,time
 from math import atan2,cos,sin,sqrt,degrees,radians
 
-L = 0
+L =  120   #hand's length 
 L1 = 300
 L2 = 250
 L3 = 160
 L4 = 72
 
-
 def send_angles(j,wait):
     with open("/run/shm/angles.tmp","w") as f:
         f.write("%d,%d,%d,%d,%d\n" % (j[0],j[1],j[2],j[3],j[4]))
         f.flush()
-
+        
     os.rename("/run/shm/angles.tmp","/run/shm/angles")
+    time.sleep(wait)
 
 def air_toggle(flag,wait):
     with open("/run/shm/ev_on_off.tmp","w") as air_f:
@@ -23,6 +23,7 @@ def air_toggle(flag,wait):
         air_f.flush()
 
     os.rename("/run/shm/ev_on_off.tmp","/run/shm/ev_on_off")
+    time.sleep(wait)
 
 def j_k(th1,th2,th3,th4,th5):
     th1,th2,th3,th4,th5 = radians(th1),radians(th2),radians(th3),radians(th4),radians(th5)
@@ -68,7 +69,16 @@ if __name__ == "__main__" :
     # send_angles(i_k(0,482,300,90,90,8,True,True),2.0)
     # air_toggle(True,2.0)
 
-    send_angles(i_k(482,0,300,0,90,8,True,True),2.0)
-    send_angles(i_k(-482,0,300,0,-90,8,True,True),2.0)
-
-
+    air_toggle(True,2.0)
+    send_angles(i_k(0,0,L1+L2+L3+L4+L,0,0,8,True,True),3.0)
+    send_angles(i_k(270,0,170,0,180,8,True,True),3.0)
+    send_angles(i_k(460,0,240,0,90,8,True,True),3.0)
+    # send_angles(i_k(460,0,180,0,90,8,True,True),1.0)
+    send_angles(i_k(460,0,100,0,90,8,True,True),3.0)
+    air_toggle(False,2.0)
+    send_angles(i_k(460,0,250,0,90,8,True,True),3.0)
+    send_angles(i_k(0,460,250,90,90,8,True,True),3.0)
+    send_angles(i_k(0,0,L1+L2+L3+L4+L,90,0,8,True,True),3.0)
+    send_angles(i_k(0,-402,600,90,-90,8,True,False),2.0)
+    send_angles(i_k(0,402,600,90,90,8,True,True),0.9)
+    air_toggle(True,2.0)
